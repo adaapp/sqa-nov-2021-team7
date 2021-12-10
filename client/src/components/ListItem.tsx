f2f617e902910596e042430ac9d9dd503da44b98
SamCrispin <samuel.crispin@ada.ac.uk>
2021-12-10 11:32:34 +0000

    WIP again

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import { DeleteButton } from './Button';
 import { TodoItem } from "../types/todo";
-import { deleteItem } from '../components/App';
+import { deleteItem } from '../services/apiservice';
 
 type ListItemProps = {
     dataTestId: string,
