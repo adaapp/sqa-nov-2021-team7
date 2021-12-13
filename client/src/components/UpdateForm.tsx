e11a4bacfcccebfddb530a7fd36a2b25e4c2d7e3
SamCrispin <samuel.crispin@ada.ac.uk>
2021-12-13 19:38:16 +0000

    Styled form

--- client/src/components/UpdateForm.tsx
+++ client/src/components/UpdateForm.tsx
@@ -16,6 +16,17 @@ const FormContainer = styled.div`
     top: 0;
     width: 100%;
     height: 100%;
+    align-content: center;
+`;
+
+const FormBox = styled.div`
+    width: 220px;
+    height: 200px;
+    background: white;
+    margin: auto;
+    margin-top: 20px;
+    border-radius: 10px;
+    border: 1px solid black;
 `;
 
 export const UpdateForm = (props: UpdateFormProps) => {
@@ -35,36 +46,53 @@ export const UpdateForm = (props: UpdateFormProps) => {
     return (
         <div style={{display: visible ? 'block' : 'none'}}>
             <FormContainer>
-                <Input
-                    placeholder={'Title'}
-                    dataTestId={'update-title'}
-                    value={title}
-                    onInput={event => {
-                        const target = event.target as HTMLInputElement;
-                        setTitle(target.value);
-                    }}
-                />
+                <FormBox>
+                    <Input
+                        placeholder={'Title'}
+                        dataTestId={'update-title'}
+                        value={title}
+                        onInput={event => {
+                            const target = event.target as HTMLInputElement;
+                            setTitle(target.value);
+                        }}
+                    />
 
-                <Input
-                    placeholder={'Description'}
-                    dataTestId={'update-description'}
-                    value={description}
-                    onInput={event => {
-                        const target = event.target as HTMLInputElement;
-                        setDescription(target.value);
-                    }}
-                />
+                    <Input
+                        placeholder={'Description'}
+                        dataTestId={'update-description'}
+                        value={description}
+                        onInput={event => {
+                            const target = event.target as HTMLInputElement;
+                            setDescription(target.value);
+                        }}
+                    />
 
-                <input
-                    type={'datetime-local'}
-                    data-test-id={'update-date-due'}
-                    onChange={event => {
-                        const target = event.target as HTMLInputElement;
-                        setDateDue(new Date(target.value).getTime());
-                    }}
-                />
+                    <input
+                        type={'datetime-local'}
+                        data-test-id={'update-date-due'}
+                        onChange={event => {
+                            const target = event.target as HTMLInputElement;
+                            setDateDue(new Date(target.value).getTime());
+                        }}
+                        style={{
+                            width: 'calc(100% - 18px)',
+                            marginLeft: '5px'
+                        }}
+                    />
 
-                <button data-test-id={'update-submit'} onClick={() => onSubmitClick()}></button>
+                    <button
+                        data-test-id={'update-submit'}
+                        onClick={() => onSubmitClick()}
+                        style={{
+                            left: '50%',
+                            marginTop: '10px',
+                            transform: 'translate(-50%)',
+                            position: 'relative'
+                        }}
+                    >
+                        Submit
+                    </button>
+                </FormBox>
             </FormContainer>
         </div>
     );
